fb13a04023bfe56ff694f7c9c01c27bb00cd115b
michele-cotrim <michelesoccotrim@gmail.com>
2025-07-13 16:52:57 -0300

    Create EventSummary.cs
    First step

---
+++ backend/QuaveChallenge.API/Models/EventSummary.cs
@@ -0,0 +1,13 @@
+namespace QuaveChallenge.API.Models
+{
+    public class EventSummary
+    {
+        public Guid CommunityId { get; set; }
+        public DateTime LastCheckin { get; set; }
+        public DateTime LastCheckout { get; set; }
+        public DateTime FirstCheckin { get; set; }
+        public DateTime FirstCheckout { get; set; }
+        public int CurrentAttendeeCount { get; set; }
+        public int PeopleNotCheckedIn { get; set; }
+    }
+}
